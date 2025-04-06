f4ef07c0267c1c45e795ea69ee7e3ceaf149a15a
kadota05 <mastnoclass@gmail.com>
2025-04-06 19:06:51 +0900

    offcanvasの位置調整

--- src/compenents/Header.tsx
+++ src/compenents/Header.tsx
@@ -2,27 +2,34 @@ import React from 'react'
 import { OffCanvas, Title } from './index'
 
 type HeaderProps = {
-    handlePastChat: (id:number) => Promise<void>; 
+  handlePastChat: (id:number) => Promise<void>;
 }
-const Header: React.FC<HeaderProps> = ( { handlePastChat } ) => {
-    return (
-        <header>
-        <nav className="navbar navbar-dark bg-dark fixed-top">
-          <div className="container-fluid d-flex align-items-center justify-content-between">
-            {/* 左側: OffCanvas トグルボタン */}
-            <div style={{ width: '50px' }}>
-              <OffCanvas handlePastChat={handlePastChat} />
-            </div>
-            {/* 中央: タイトル */}
-            <div className="text-center flex-grow-1">
-              <Title />
-            </div>
-            {/* 右側: 空要素（左側と同じ幅でタイトルを中央に配置） */}
-						<></>
+
+const Header: React.FC<HeaderProps> = ({ handlePastChat }) => {
+  return (
+    <header>
+      <nav className="navbar navbar-dark bg-dark fixed-top">
+        {/* d-flex でFlexbox化し、3つの要素に .flex-fill を指定 */}
+        <div className="container-fluid d-flex align-items-center">
+          
+          {/* 左側 */}
+          <div className="flex-fill text-center">
+            <OffCanvas handlePastChat={handlePastChat} />
           </div>
-        </nav>
-      </header>
-    )
+          
+          {/* 中央 */}
+          <div className="flex-fill text-center">
+            <Title />
+          </div>
+          
+          {/* 右側 */}
+          <div className="flex-fill text-center">
+            {/* 必要なら何か配置 */}
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header;
